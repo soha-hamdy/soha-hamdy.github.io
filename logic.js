0e0d5bb40d3c32ebc81d51aba7ae2a79df2fb377
Soha Hamdy <71373850+soha-hamdy@users.noreply.github.com>
2020-09-16 13:45:09 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
-var birthday = new Date("09/09/2001");
 var today = new Date();
-var difference = today.getTime() - birthday.getTime();
-var years = difference / (365 * 24 * 3600 * 1000);
-difference = difference % (365 * 24 * 3600 * 1000);
-var days = difference / (24 * 3600 * 1000);
-document.getElementById("how_old").innerHTML = "years: " + years.toString() + "days: " + days.toString();
+var birthday = moment([2001, 9, 9]);
+var curr_day = moment([today.getFullYear(), today.getMonth(), today.getDate()]);
+var years = birthday.diff(curr_day, 'years');
+var months = birthday.diff(curr_day, 'months');
+var days = birthday.diff(curr_day, 'days');
+document.getElementById("how_old").innerHTML = "years: " + years.toString() + "months: " + months.toString() + "days: " + days.toString();
