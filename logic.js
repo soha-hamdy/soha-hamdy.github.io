876dd7cfa4467937c30c9a538414564a7946ce07
Soha Hamdy <71373850+soha-hamdy@users.noreply.github.com>
2021-08-31 17:48:58 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,10 +1,78 @@
-var birthday = moment([2001, 9, 9]);
-var today = new Date();
-var curr_day = moment([today.getFullYear(), today.getMonth() + 1, today.getDate()]);
-var duration = moment.duration(curr_day.diff(birthday));
-var years = duration.years();
-var months = duration.months();
-var days = duration.days();
+function getAge(dateString) {
+  var now = new Date();
+  var today = new Date(now.getYear(),now.getMonth(),now.getDate());
+
+  var yearNow = now.getYear();
+  var monthNow = now.getMonth();
+  var dateNow = now.getDate();
+
+  var dob = new Date(dateString.substring(6,10),
+                     dateString.substring(0,2)-1,                   
+                     dateString.substring(3,5)                  
+                     );
+
+  var yearDob = dob.getYear();
+  var monthDob = dob.getMonth();
+  var dateDob = dob.getDate();
+  var age = {};
+  var ageString = "";
+  var yearString = "";
+  var monthString = "";
+  var dayString = "";
+
+
+  yearAge = yearNow - yearDob;
+
+  if (monthNow >= monthDob)
+    var monthAge = monthNow - monthDob;
+  else {
+    yearAge--;
+    var monthAge = 12 + monthNow -monthDob;
+  }
+
+  if (dateNow >= dateDob)
+    var dateAge = dateNow - dateDob;
+  else {
+    monthAge--;
+    var dateAge = 31 + dateNow - dateDob;
+
+    if (monthAge < 0) {
+      monthAge = 11;
+      yearAge--;
+    }
+  }
+
+  age = {
+      years: yearAge,
+      months: monthAge,
+      days: dateAge
+      };
+
+
+  if ( (age.years > 0) && (age.months > 0) && (age.days > 0) )
+    ageString = age.years + yearString + " " + age.months + monthString + " " + age.days + dayString;
+  else if ( (age.years == 0) && (age.months == 0) && (age.days > 0) )
+    ageString = "Only " + age.days + dayString;
+  else if ( (age.years > 0) && (age.months == 0) && (age.days == 0) )
+    ageString = age.years + yearString;
+  else if ( (age.years > 0) && (age.months > 0) && (age.days == 0) )
+    ageString = age.years + yearString + age.months + monthString;
+  else if ( (age.years == 0) && (age.months > 0) && (age.days > 0) )
+    ageString = age.months + monthString + age.days + dayString;
+  else if ( (age.years > 0) && (age.months == 0) && (age.days > 0) )
+    ageString = age.years + yearString + age.days + dayString;
+  else if ( (age.years == 0) && (age.months > 0) && (age.days == 0) )
+    ageString = age.months + monthString;
+  else ageString = "Oops! Could not calculate age!";
+
+  return ageString;
+}
+
+
+var soha_age = getAge('09/09/2001').split(" ");
+var years = soha_age[0];
+var months = soha_age[1];
+var days = soha_age[2];
 document.getElementById("years").innerHTML = years;
 document.getElementById("months").innerHTML = months;
 document.getElementById("days").innerHTML = days;
